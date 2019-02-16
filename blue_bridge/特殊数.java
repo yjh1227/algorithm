88ab220d4f9d6fa244c249596804013febe8881b
yjh <1254094880@qq.com>
2019-02-16 13:21:55 +0800

    blue_bridge

---
+++ blue_bridge/特殊数.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=0;
+        int b=0;
+        int c=0;
+        for (int i=100;i<=999;i++){
+            a=i/100;
+            b=i/10%10;
+            c=i%10;
+            if (a*a*a+b*b*b+c*c*c==i){
+                System.out.println(i);
+            }
+        }
+    }
+}
