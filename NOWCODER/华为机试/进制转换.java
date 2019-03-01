fb1369f0ba4cebd4a98b8089db2071da4261d65f
Junhui Yu <1254094880@qq.com>
2019-03-01 16:03:48 +0800

    发现了方法二的错误,并进行了改进,还有进行输入多多用hasNext

--- NOWCODER/华为机试/进制转换.java
+++ NOWCODER/华为机试/进制转换.java
@@ -72,3 +72,14 @@ public class 进制转换{
 //        System.out.println(n);
 //    }
 //}
+//方法二改进:
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while (sc.hasNext()){//使用hasNext比较好,以后多多注意.
+            String str=sc.next().substring(2);
+            System.out.println(Integer.parseInt(str,16));
+        }
+    }
+} 
