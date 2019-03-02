2df52a6319940db3daa4e7d750a8e13b0f89d7ba
Junhui Yu <1254094880@qq.com>
2019-03-02 19:40:03 +0800

    Create 字符串匹配.java

---
+++ NOWCODER/华为机试/字符串匹配.java
@@ -0,0 +1,63 @@
+package LeetCode;
+/*
+题目描述
+题目标题：
+
+判断短字符串中的所有字符是否在长字符串中全部出现
+
+详细描述：
+
+接口说明
+
+原型：
+
+boolIsAllCharExist(char* pShortString,char* pLongString);
+
+输入参数：
+
+    char* pShortString：短字符串
+
+    char* pLongString：长字符串
+
+
+
+
+
+
+
+
+
+输入描述:
+输入两个字符串。第一个为短字符，第二个为长字符。
+
+输出描述:
+返回值：
+
+示例1
+输入
+bc
+abc
+输出
+true
+ */
+import java.util.Scanner;
+
+public class 字符串匹配 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()){
+            String sho=sc.nextLine();
+            String lon=sc.nextLine();
+            System.out.println(IsMath(sho,lon));
+        }
+    }
+
+    private static Boolean IsMath(String sho, String lon) {
+        for (int i=0;i<sho.length();i++){
+            if (!lon.contains(Character.toString(sho.charAt(i)))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
