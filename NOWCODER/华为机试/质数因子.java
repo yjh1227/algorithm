4245f50fbae22b3f7f980dc33d35fe6001f3ff5a
Junhui Yu <1254094880@qq.com>
2019-03-03 15:09:46 +0800

    增加了方法二

--- NOWCODER/华为机试/质数因子.java
+++ NOWCODER/华为机试/质数因子.java
@@ -55,3 +55,45 @@ public class 质数因子 {
 
     }
 }
+
+
+
+/*
+方法二:不使用链表
+
+import java.util.Scanner;
+ 
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+         
+        long number = 0;
+         
+        while(scanner.hasNextLong())
+        {
+            number = scanner.nextLong();
+            isPrimerFactors(number);
+        }
+         
+    }
+     
+    private static void isPrimerFactors(long num)
+    {
+        long number = num;
+        while(number != 1)
+        {
+            for(int i = 2; i <= number ;i++)
+            {
+                if(number % i == 0)
+                {
+                    number /= i;
+                    System.out.print(i + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
+*/
