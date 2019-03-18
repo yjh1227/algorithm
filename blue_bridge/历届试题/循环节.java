247df20278163f782e9e401cde036026bf4ec028
Junhui Yu <1254094880@qq.com>
2019-03-18 21:56:18 +0800

    Create 循环节.java

---
+++ blue_bridge/历届试题/循环节.java
@@ -0,0 +1,27 @@
+package Test;
+
+import java.util.Vector;
+
+public class 循环节 {
+    public static void main(String[] args) {
+        System.out.println(f(11,13));
+    }
+
+    public static int f(int n, int m)
+    {
+        n = n % m;
+        Vector v = new Vector();
+
+        for(;;)
+        {
+            v.add(n);
+            n *= 10;
+            n = n % m;
+            if(n==0)
+                return 0;
+            if(v.indexOf(n)>=0){
+                return v.size();
+            }
+        }
+    }
+}
